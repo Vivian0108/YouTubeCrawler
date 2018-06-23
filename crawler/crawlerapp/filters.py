913b2637a4c04629a0bf846c9586b24c0b2a0e6d
ASchneidman <alexmarkschneidman@gmail.com>
2018-06-23 17:23:18 -0400

    bugs

--- crawler/crawlerapp/filters.py
+++ crawler/crawlerapp/filters.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 import random
-import time, os
+import time, os, subprocess
 from crawlerapp.definitions import CONFIG_PATH
 #from AZP2FA.p2fa.align_mod import align
 
@@ -56,31 +56,28 @@ class AlignFilter(AbstractFilter):
     def filter(self, video_ids, download_path):
         for video in video_ids:
             print("Trying " + video)
-            try:
-                video_dir = os.path.join(download_path, video)
-                mp4_path = os.path.join(video_dir, video + ".mp4")
-                vtt_path = os.path.join(video_dir, video + ".en.vtt")
-                plaintext_path = os.path.join(video_dir, video + ".plaintext")
-                wav_path = os.path.join(video_dir, video + ".wav")
-                pratt_path = os.path.join(video_dir, video + ".pratt")
-                output_file = os.path.join(video_dir, video + ".TextGrid")
-                align_path = os.path.join(CONFIG_PATH, os.path.join("AZP2FA", os.path.join("p2fa", os.path.join("align.py"))))
-                lines = []
-                with open(vtt_path, 'r') as f:
-                    for line in f:
-                        remove_cond = (
-                            'WEBVTT' in line or
-                            'Kind: captions' in line or
-                            'Language: en' in line or
-                            '-->' in line
-                        )
-                        if not remove_cond:
-                            lines.append(line.strip())
+            video_dir = os.path.join(download_path, video)
+            mp4_path = os.path.join(video_dir, video + ".mp4")
+            vtt_path = os.path.join(video_dir, video + ".en.vtt")
+            plaintext_path = os.path.join(video_dir, video + ".plaintext")
+            wav_path = os.path.join(video_dir, video + ".wav")
+            pratt_path = os.path.join(video_dir, video + ".pratt")
+            output_file = os.path.join(video_dir, video + ".TextGrid")
+            align_path = os.path.join(CONFIG_PATH, os.path.join("AZP2FA", os.path.join("p2fa", os.path.join("align.py"))))
+            lines = []
+            with open(vtt_path, 'r') as f:
+                for line in f:
+                    remove_cond = (
+                        'WEBVTT' in line or
+                        'Kind: captions' in line or
+                        'Language: en' in line or
+                        '-->' in line
+                    )
+                    if not remove_cond:
+                        lines.append(line.strip())
 
-                with open(plaintext_path, 'w') as f:
-                    f.write(' '.join(lines))
-                subprocess.call(align_path + ' %s %s %s'
-                                % (wav_path, plaintext_path, pratt_path),
-                                shell=True)
-            except:
-                print("Failed video " + video)
+            with open(plaintext_path, 'w') as f:
+                f.write(' '.join(lines))
+            subprocess.call(align_path + ' %s %s %s'
+                            % (wav_path, plaintext_path, pratt_path),
+                            shell=True)
