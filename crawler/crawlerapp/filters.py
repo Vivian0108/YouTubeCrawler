a9c368e7f3491788832b61ed5560d85b75b3fc97
ASchneidman <alexmarkschneidman@gmail.com>
2018-06-23 17:26:44 -0400

    excep

--- crawler/crawlerapp/filters.py
+++ crawler/crawlerapp/filters.py
@@ -56,28 +56,31 @@ class AlignFilter(AbstractFilter):
     def filter(self, video_ids, download_path):
         for video in video_ids:
             print("Trying " + video)
-            video_dir = os.path.join(download_path, video)
-            mp4_path = os.path.join(video_dir, video + ".mp4")
-            vtt_path = os.path.join(video_dir, video + ".en.vtt")
-            plaintext_path = os.path.join(video_dir, video + ".plaintext")
-            wav_path = os.path.join(video_dir, video + ".wav")
-            pratt_path = os.path.join(video_dir, video + ".pratt")
-            output_file = os.path.join(video_dir, video + ".TextGrid")
-            align_path = os.path.join(CONFIG_PATH, os.path.join("AZP2FA", os.path.join("p2fa", os.path.join("align.py"))))
-            lines = []
-            with open(vtt_path, 'r') as f:
-                for line in f:
-                    remove_cond = (
-                        'WEBVTT' in line or
-                        'Kind: captions' in line or
-                        'Language: en' in line or
-                        '-->' in line
-                    )
-                    if not remove_cond:
-                        lines.append(line.strip())
+            try:
+                video_dir = os.path.join(download_path, video)
+                mp4_path = os.path.join(video_dir, video + ".mp4")
+                vtt_path = os.path.join(video_dir, video + ".en.vtt")
+                plaintext_path = os.path.join(video_dir, video + ".plaintext")
+                wav_path = os.path.join(video_dir, video + ".wav")
+                pratt_path = os.path.join(video_dir, video + ".pratt")
+                output_file = os.path.join(video_dir, video + ".TextGrid")
+                align_path = os.path.join(CONFIG_PATH, os.path.join("AZP2FA", os.path.join("p2fa", os.path.join("align.py"))))
+                lines = []
+                with open(vtt_path, 'r') as f:
+                    for line in f:
+                        remove_cond = (
+                            'WEBVTT' in line or
+                            'Kind: captions' in line or
+                            'Language: en' in line or
+                            '-->' in line
+                        )
+                        if not remove_cond:
+                            lines.append(line.strip())
 
-            with open(plaintext_path, 'w') as f:
-                f.write(' '.join(lines))
-            subprocess.call(align_path + ' %s %s %s'
-                            % (wav_path, plaintext_path, pratt_path),
-                            shell=True)
+                with open(plaintext_path, 'w') as f:
+                    f.write(' '.join(lines))
+                subprocess.call(align_path + ' %s %s %s'
+                                % (wav_path, plaintext_path, pratt_path),
+                                shell=True)
+            except FileNotFoundError as e:
+                print(e)
