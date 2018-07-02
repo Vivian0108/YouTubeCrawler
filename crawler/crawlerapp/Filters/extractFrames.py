c914a7599f8e0fd0d044be6dad24a8f88a097ea8
ASchneidman <alexmarkschneidman@gmail.com>
2018-07-02 14:26:13 -0400

    indents

--- crawler/crawlerapp/Filters/extractFrames.py
+++ crawler/crawlerapp/Filters/extractFrames.py
@@ -7,19 +7,19 @@ def extractFrames(videoID, FPS, video_path):
 	vidcap = cv2.VideoCapture(video_path + ".mp4")
 	success, image = vidcap.read()
 	curr = 0
-    fcount = 1
-    # wait = 1.0/FPS
-    # numFrames = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
-    vidFPS = vidcap.get(cv2.CAP_PROP_FPS)
-    # numSecs = int (numFrames/vidFPS)
-    FPS = int((vidFPS / FPS))
-    while success:
-        if curr == 0:
-            os.mkdir(frames_path)
-        success, image = vidcap.read()
-        if curr % FPS == 0:
+	fcount = 1
+	# wait = 1.0/FPS
+	# numFrames = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
+	vidFPS = vidcap.get(cv2.CAP_PROP_FPS)
+	# numSecs = int (numFrames/vidFPS)
+	FPS = int((vidFPS / FPS))
+	while success:
+		if curr == 0:
+			os.mkdir(frames_path)
+		success, image = vidcap.read()
+		if curr % FPS == 0:
 			write_path = os.path.join(frames_path,videoID)
-            cv2.imwrite(write_path + '%03d.jpg' % fcount, image)
-        	fcount += 1
-    	curr += 1
+			cv2.imwrite(write_path + '%03d.jpg' % fcount, image)
+			fcount += 1
+		curr += 1
 	vidcap.release()
