48f0a22eaf17ba65c68ea1317ab574a48b00babc
ASchneidman <alexmarkschneidman@gmail.com>
2018-08-01 14:39:41 -0400

    catching exceptions

--- crawler/crawlerapp/models.py
+++ crawler/crawlerapp/models.py
@@ -45,12 +45,15 @@ class Job(models.Model):
     # Deletes job and and videos crawled by job if that video only was crawled by this job
     def deleteJob(self):
         for video_id in self.videos:
-            vid_query = Video.objects.filter(id=video_id).get()
-            if (str(self.id) in vid_query.job_ids) and (len(vid_query.job_ids) > 1):
-                vid_query.job_ids.remove(str(self.id))
-                vid_query.save()
-            else:
-                vid_query.delete()
+            try:
+                vid_query = Video.objects.filter(id=str(video_id)).get()
+                if (str(self.id) in vid_query.job_ids) and (len(vid_query.job_ids) > 1):
+                    vid_query.job_ids.remove(str(self.id))
+                    vid_query.save()
+                else:
+                    vid_query.delete()
+            except Exception as e:
+                print("Couldn't delete video " + str(video_id))
         all_datasets = list(Dataset.objects.all())
         for d in all_datasets:
             if str(self.id) in d.jobs_list:
