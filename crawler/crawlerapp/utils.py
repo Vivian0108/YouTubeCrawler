b4001e4fda52f5eec921280ffa958ed053b9a39a
ASchneidman <alexmarkschneidman@gmail.com>
2018-08-03 13:35:49 -0400

    fixing translate

--- crawler/crawlerapp/utils.py
+++ crawler/crawlerapp/utils.py
@@ -131,6 +131,6 @@ def job_update(job_id):
 
 def translate(query, language):
     translator= Translator()
-    if ((language == 'any') or (translator.detect(query.lang) == language) or (language == '')):
+    if ((language == 'any') or (translator.detect(query) == language) or (language == '')):
         return query
     return translator.translate(query, dest=language)
