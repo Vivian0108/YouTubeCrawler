8c0ea911335076098fe5fa848949ca2769d9f6d5
ASchneidman <alexmarkschneidman@gmail.com>
2018-10-18 16:57:41 -0400

    Updated download path to downloaded_videos

--- crawler/crawlerapp/definitions.py
+++ crawler/crawlerapp/definitions.py
@@ -3,4 +3,4 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # This is your Project Root
 CONFIG_PATH = os.path.join(ROOT_DIR)
 
-CRAWLED_VIDEOS_DIR = '/media/crawlhdd1/'
+CRAWLED_VIDEOS_DIR = os.path.join(CONFIG_PATH,'downloaded_videos')
