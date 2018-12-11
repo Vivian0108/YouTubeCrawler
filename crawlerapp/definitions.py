28e8f36437829c57490545ebba892651196f613e
ASchneidman <alexmarkschneidman@gmail.com>
2018-12-10 22:01:32 -0500

    Moved all files into crawler/ directory to make project focused on django app

---
+++ crawlerapp/definitions.py
@@ -0,0 +1,7 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # This is your Project Root
+CONFIG_PATH = os.path.join(ROOT_DIR)
+
+#CRAWLED_VIDEOS_DIR = os.path.join(CONFIG_PATH,'downloaded_videos')
+CRAWLED_VIDEOS_DIR = 'downloaded_videos'
